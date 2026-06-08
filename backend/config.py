a9ca96e8ccd31d379ba6dace94921a668e8865ba
1anuragsingh <rajbnna364@gmail.com>
2026-06-08 16:32:13 +0530

    apollo api key

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ EAZYREACH_API_KEY = os.getenv("EAZYREACH_API_KEY", "afa2fe6b-e75c-4fd9-8583-b8f4
 BREVO_API_KEY = os.getenv("BREVO_API_KEY", "xsmtpsib-bee0173d2b4050891f5ec835d873b90df12316f795b4b2e7c4fd2c3bcf1b2ce4-Z0SjcxMwQ5GJHwWKb ")
 
 SENDER_NAME = os.getenv("SENDER_NAME", "Anurag Singh")
-SENDER_COMPANY = os.getenv("SENDER_COMPANY", "Your Company")
-SENDER_EMAIL = os.getenv("SENDER_EMAIL", "you@yourdomain.com")
+SENDER_COMPANY = os.getenv("SENDER_COMPANY", "Self")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL", "rajbnna364@gmail.com")
